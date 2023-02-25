7fc884010acbe8b44a9b8b375af87fff51c93854
Stepan <stepandorhin@yandex.ru>
2023-02-25 16:50:19 +0300

    Fix mixed up bittons

--- nvim/lua/bfswrd/remap.lua
+++ nvim/lua/bfswrd/remap.lua
@@ -28,8 +28,8 @@ map("n", "<C-u>", "<C-u>zz")
 
 -- Перескакиваем по окнам
 map('n', '<Left>', '<C-w>h')
-map('n', '<Down>', '<C-w>k')
-map('n', '<Up>', '<C-w>j')
+map('n', '<Down>', '<C-w>j')
+map('n', '<Up>', '<C-w>k')
 map('n', '<Right>', '<C-w>l')
 
 map("n", "<leader>x", ":bd<CR>")
