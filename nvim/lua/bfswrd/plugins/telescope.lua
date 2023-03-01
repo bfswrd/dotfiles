57976104c45d5631a4140411b48130931e929f33
Stepan <stepandorhin@yandex.ru>
2023-03-01 11:28:02 +0300

    Update telescope. Add initial_mode normal

--- nvim/lua/bfswrd/plugins/telescope.lua
+++ nvim/lua/bfswrd/plugins/telescope.lua
@@ -1,4 +1,11 @@
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>b', builtin.buffers, {})
+require("telescope").setup({
+	defaults = {
+		initial_mode = "normal",
+	},
+})
+
+local builtin = require("telescope.builtin")
+
+vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+vim.keymap.set("n", "<leader>b", builtin.buffers, {})
