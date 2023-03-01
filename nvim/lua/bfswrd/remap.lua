a0556c2e5dde4862184920ec44ce7c6e1cb28377
Stepan <stepandorhin@yandex.ru>
2023-03-01 11:18:04 +0300

    Update remap

--- nvim/lua/bfswrd/remap.lua
+++ nvim/lua/bfswrd/remap.lua
@@ -1,7 +1,9 @@
-function map(mode, lhs, rhs, opts)
-    local options = { noremap = true, silent = true }
-    if opts then options = vim.tbl_extend('force', options, opts) end
-    vim.keymap.set(mode, lhs, rhs, options)
+local function map(mode, lhs, rhs, opts)
+	local options = { noremap = true, silent = true }
+	if opts then
+		options = vim.tbl_extend("force", options, opts)
+	end
+	vim.keymap.set(mode, lhs, rhs, options)
 end
 
 map("", "<Space>", "<Nop>")
@@ -20,22 +22,22 @@ map("n", "n", "nzzzv")
 map("n", "N", "Nzzzv")
 
 -- Splits
-map('n', '<leader>v', '<C-w>v<C-w>l') -- open a vertical split and switch to it
-map('n', '<leader>h', '<C-w>s<C-w>j') -- open a horizontal split and switch to it
+map("n", "<leader>v", "<C-w>v<C-w>l") -- open a vertical split and switch to it
+map("n", "<leader>h", "<C-w>s<C-w>j") -- open a horizontal split and switch to it
 
 map("n", "<C-d>", "<C-d>zz")
 map("n", "<C-u>", "<C-u>zz")
 
 -- Перескакиваем по окнам
-map('n', '<Left>', '<C-w>h')
-map('n', '<Down>', '<C-w>j')
-map('n', '<Up>', '<C-w>k')
-map('n', '<Right>', '<C-w>l')
+map("n", "<Left>", "<C-w>h")
+map("n", "<Down>", "<C-w>j")
+map("n", "<Up>", "<C-w>k")
+map("n", "<Right>", "<C-w>l")
 
 map("n", "<leader>x", ":bd<CR>")
 
 -- Переключает чиселки
-map('n', '<F10>', ':set number! relativenumber! relativenumber?<CR>')
+map("n", "<F10>", ":set number! relativenumber! relativenumber?<CR>")
 
 -- next greatest remap ever : asbjornHaland
 map({ "n", "v" }, "<leader>y", [["+y]])
@@ -57,5 +59,4 @@ map("n", "<leader><leader>", ":noh<CR>")
 map("n", "<leader>E", "<cmd> NvimTreeToggle <CR>")
 map("n", "<leader>e", "<cmd> NvimTreeFocus <CR>")
 
-map("n", "<leader>n", "<cmd> ASToggle <CR>", {silent = false})
-
+map("n", "<leader>n", "<cmd> ASToggle <CR>", { silent = false })
